03afadf52bd874f20bd92eaa6d815ea6f6d557bb
vroa74 <33233830+vroa74@users.noreply.github.com>
2025-10-20 17:43:48 -0600

    up000009

--- database/migrations/2025_10_13_035859_create_post_table.php
+++ database/migrations/2025_10_13_035859_create_post_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('slug')->nullable()->unique();
             $table->longText('body');
+            $table->timestamp('published_at');
             $table->timestamps();
         });
     } 
